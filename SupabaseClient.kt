8cc808496d128eb8fed214ee05f8508fc6dab0e2
dhiaaalshekh781-code <dhiaaalshekh781@gmail.com>
2026-07-18 23:27:29 +0300

    Remove SupabaseClient implementation
    Removed the SupabaseClient object and its related imports.

--- SupabaseClient.kt
+++ SupabaseClient.kt
@@ -1,13 +1,1 @@
-package com.example.myapp
 
-import io.github.jan_tennert.supabase.createSupabaseClient
-import io.github.jan_tennert.supabase.postgrest.Postgrest
-
-object SupabaseClient {
-    val client = createSupabaseClient(
-        supabaseUrl = "https://bexlpwqzmwaloafnrrle.supabase.co",
-        supabaseKey = "sb_publishable_GiIkF-_TW4vmTRnYbeQ49w_8_fx0PjL"
-    ) {
-        install(Postgrest)
-    }
-}
